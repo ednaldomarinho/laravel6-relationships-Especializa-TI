c7a5e6f86a5431fe115d1c2300fd769a76977799
ednaldomarinho <ednaldosmarinho@gmail.com>
2020-08-26 17:06:30 +0000

    Lesson 17 - Polymorfic Relationship

---
+++ app/Http/Controllers/PolymorficController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PolymorficController extends Controller
+{
+    public function polymorfic()
+    {
+        echo "test";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,11 @@ Route::get('/many-to-many-inverse', 'ManyToManyController@manyToManyInverse')->n
 // Route::get('/one-to-many-two', 'OneToManyController@oneToManyTwo')->name('onetomanytwo');
 Route::get('/many-to-many-insert', 'ManyToManyController@manyToManyInsert')->name('manytomanyinsert');
 // Route::get('/one-to-many-insert-two', 'OneToManyController@oneToManyInsertTwo')->name('onetomanyinserttwo');
+
+/* Relation Polymorfic */
+
+Route::get('/polymorfics', 'PolymorficController@polymorfic')->name('polymorfic');
+// Route::get('/many-to-many-inverse', 'ManyToManyController@manyToManyInverse')->name('manytomanyinverse');
+// // Route::get('/one-to-many-two', 'OneToManyController@oneToManyTwo')->name('onetomanytwo');
+// Route::get('/many-to-many-insert', 'ManyToManyController@manyToManyInsert')->name('manytomanyinsert');
+// // Route::get('/one-to-many-insert-two', 'OneToManyController@oneToManyInsertTwo')->name('onetomanyinserttwo');
