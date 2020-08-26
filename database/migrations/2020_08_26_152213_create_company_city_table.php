3a5ef23a9ebb68c25833da3e007dfe22ebc8c4f8
ednaldomarinho <ednaldosmarinho@gmail.com>
2020-08-26 15:33:34 +0000

    Lesson 13 - Create Many-to-Many Tables

---
+++ database/migrations/2020_08_26_152213_create_company_city_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyCityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_city', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('city_id')->unsigned();
+            $table->foreign('city_id')
+                    ->references('id')
+                    ->on('cities')
+                    ->onDelete('cascade');
+
+            $table->bigInteger('company_id')->unsigned();
+            $table->foreign('company_id')
+                    ->references('id')
+                    ->on('companies')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_city');
+    }
+}
